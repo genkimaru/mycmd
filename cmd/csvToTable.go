ec40316b2954af8286b84123e98f10f1966c74b1
guan.c.wang <guan.c.wang@accenture.com>
2019-12-23 13:51:55 +0800

    add simple command

--- cmd/csvToTable.go
+++ cmd/csvToTable.go
@@ -17,7 +17,6 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
@@ -32,6 +31,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
+	fmt.Println(cmd.Short)
 		fmt.Println("csvToTable called")
 	},
 }
